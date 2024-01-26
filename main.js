bc38e7c8dda40ecffe687e965895e38231c82239
ungya <68705733+ungya@users.noreply.github.com>
2024-01-27 04:46:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings: ["Frontend Devloper" , "Backend devloper" , "Web Devloper"],
+    strings: ["Frontend Devloper" , "Backend devloper" , "Web Devloper" , "Software Engineer"],
     typeSpeed : 100,
     backSpeed: 100,
     backDelay:1000,
